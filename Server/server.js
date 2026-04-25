676b81a89e1d5baedf3987d17341c75241ec9f7c
ujjwal <ujjwal.tuteja03@gmail.com>
2026-04-25 23:21:35 +0530

    fixed route syntax

--- Server/server.js
+++ Server/server.js
@@ -135,7 +135,7 @@ app.post("/api/v1/restaurants/:id/addReview", async (req, res) => {
 app.use(express.static(path.join(__dirname, ".." , "client", "build")));
 
 // The "Catch-all" handler: for any request that doesn't match an API route, send back React's index.html file.
-app.get("*", (req, res) => {
+app.get(".*", (req, res) => {
   res.sendFile(path.join(__dirname, ".." , "client", "build", "index.html"));
 });
 
