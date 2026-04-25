3d21eb87540d69c4d8e5ada423d44051a9c95d6d
ujjwal <ujjwal.tuteja03@gmail.com>
2026-04-26 00:38:40 +0530

    chore: path logic fix

--- Server/server.js
+++ Server/server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3001;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const buildPath = path.join(__dirname, "client", "build");
+const buildPath = path.join(__dirname, "build");
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
